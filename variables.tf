035e8e41e6ffa7344be4ef9a894093268d49a0be
Maxime Poullain <maxime.poullain@fr.clara.net>
2023-01-16 13:57:55 +0100

    AZ-978: improve pip custom label name description

--- variables.tf
+++ variables.tf
@@ -53,7 +53,7 @@ variable "public_ip_allocation_method" {
 }
 
 variable "public_ip_custom_domain_name_label" {
-  description = "Label for the Domain Name. Will be used to make up the FQDN. If a domain name label is specified, an A DNS record is created for the public IP in the Microsoft Azure DNS system."
+  description = "Label for the Domain Name. Will be used to make up the FQDN. If a domain name label is specified, an A DNS record is created for the public IP in the Microsoft Azure DNS system. Defaults to Load Balancer's name, set `null` to disable."
   type        = string
   default     = ""
 }
